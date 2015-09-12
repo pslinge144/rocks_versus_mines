1f40464b012ddb441793d01bdb8c172106ba53df
Philip Slingerland <pslinge144@gmail.com>
2015-09-12 14:36:01 -0400

    added data characterization scripts for data set

---
+++ rockVmineSummaries.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+import sys
+
+# read data from uci data repository
+target_url = ("https://archive.ics.uci.edu/ml/machine-learning-"
+"databases/undocumented/connectionist-bench/sonar/sonar.all-data")
+
+data = urlopen(target_url)
+
+# arrange data into list for labels and list of lists for attributes
+xList = []
+labels = []
+for line in data:
+	# split on comma
+	row = line.strip().split(b",")
+	xList.append(row)
+
+sys.stdout.write("Number of Rows of Data = " + str(len(xList)) + '\n')
+sys.stdout.write("Number of Columns of Data = " + str(len(xList[1])) + '\n')
