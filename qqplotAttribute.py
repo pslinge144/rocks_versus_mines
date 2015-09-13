2c0cbe1efd798953b05713faed98eb816f2a5e7d
Philip Slingerland <pslinge144@gmail.com>
2015-09-13 07:03:12 -0400

    Added QQ plot to get a sense of distribution of data relative to normal

---
+++ qqplotAttribute.py
@@ -0,0 +1,33 @@
+from urllib.request import urlopen
+import sys
+import numpy as np
+import pylab
+import scipy.stats as stats
+
+# read data from uci data repository
+target_url = ("https://archive.ics.uci.edu/ml/machine-learning-"
+"databases/undocumented/connectionist-bench/sonar/sonar.all-data")
+data = urlopen(target_url)
+
+# arrange data into list for labels and list of lists for attributes
+xList = []
+labels = []
+
+for line in data:
+	# split on comma
+	row = line.strip().split(b",")
+	xList.append(row)
+nrow = len(xList)
+ncol = len(xList[1])
+
+type = [0]*3
+colCounts = []
+
+# generate summary statistics for column 3 (e.g.)
+col = 3
+colData = []
+for row in xList:
+	colData.append(float(row[col]))
+
+stats.probplot(colData, dist="norm", plot=pylab)
+pylab.show()
